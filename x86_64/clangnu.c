b183af3c1d7662b1eae7fab21def8985b869431f
Shimushushushu <wangsijie@outlook.com>
2019-04-06 17:32:02 +0800

    Add x86_64 support

---
+++ x86_64/clangnu.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char target[1024] = "clang -target x86_64-pc-windows-gnu ";
+
+int main(int argc, char* argv[]) {
+	for(int i=2; i <= argc; i++) {
+		strcat(target, argv[i-1]);
+		strcat(target, " ");
+	}
+	printf("%s\n", target);
+	system(target);
+	return 0;
+}
